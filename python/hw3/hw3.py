11e0d55d3a4504aa240dfdc78609d2a929beecd3
jashook <jshook@smu.edu>
2013-03-24 12:41:12 -0500

    u

--- python/hw3/hw3.py
+++ python/hw3/hw3.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 ################################################################################
 ################################################################################
 #
@@ -36,6 +37,16 @@ def getLinks(url,baseurl="http://lyle.smu.edu/~tylerm/courses/cse3353/webtravers
    #pull out the links from the HTML and return
    return [baseurl+a["href"].encode('ascii','ignore') for a in soup.findAll('a')]
 
+def get_path(_Node, _List = None):
+
+   if _List is None: _List = list()
+
+   _List.append(_Node[0]) # append child
+
+   if _Node[1] is not None: _List = get_path(_Node[1], _List) # append parent 
+
+   return _List
+
 def dfs(url, _PassedList = None, _Set = None):
    """
    Recursive helper function that returns a list of urls
@@ -47,11 +58,9 @@ def dfs(url, _PassedList = None, _Set = None):
    ######################################################################
 
    if _Set is None: _Set = set() 
-   if _PassedList is None: _List = list()
+   if _PassedList is None: _PassedList = list()
 
    _Set.add(url)
-
-   _PassedList.append(url)
 
    _List = getLinks(url) # get all of the links from that url
 
@@ -61,9 +70,11 @@ def dfs(url, _PassedList = None, _Set = None):
       # if the url is in the set then skip over it
       ######################################################################
 
-      if _Url not in _Set: continue
+      if _Url in _Set: continue
 
       dfs(_Url, _PassedList, _Set)
+   
+      _PassedList.append(_Url)
 
    return _PassedList
 
@@ -73,33 +84,78 @@ def print_dfs(url):
    in depth-first order
    """
   
+   print "\n" # help make everything more readable
+
    print dfs(url)
 
-def bfs(url, _List = None, _Queue = None, _Set = None):
+   print "\n"
+
+def bfs(url, _SearchTerm = None, _PassedList = None, _Queue = None, _Set = None, _Parent = None):
    """
    Recursive helper function that returns a list of urls
    in depth-first order
    """
 
-   if _List is None: _List = list()
+   if _PassedList is None: _PassedList = list()
    if _Set is None: _Set = set()
-   if _Queue is None: _Queue = queue()
+   if _Queue is None: _Queue = deque()
    
-   _Set.add(url)
+   if _Parent is None: url = (url, None)
 
-   _List.append(url)
+   else: url = (url, _Parent) 
 
-   _List = getLinks(url) # get all of the links from that url
-
-   for _Url in _List:
+   if url[0] not in _Set: 
 
       ######################################################################
-      # if the url is in the set then skip over it
+      # Checking if in set because there may be repeats in the queue
       ######################################################################
 
-      if _Url not in _Set: continue
+      _PassedList.append(url[0])
 
-      bfs(_Url, _PassedList, _Set)
+      _Set.add(url[0])
+
+      _Queue.append(url)
+
+   _List = getLinks(url[0]) # get all of the links from that url
+
+   _List = [ (_Index, url) for _Index in _List ]
+
+   _Extended = False # boolean to make sure the queue is only added to once
+
+   while _Queue:
+
+      if not _Extended: 
+         
+         _Queue.extend(_List)
+         _Extended = True # recursion will set the value to false on the next call
+
+      _ToBeProcessed = _Queue.popleft() # url to be processed
+
+      if _ToBeProcessed[0] in _Set: continue
+
+      _Set.add(_ToBeProcessed[0])
+
+      _PassedList.append(_ToBeProcessed[0])
+
+      if _SearchTerm is not None:
+
+         if _ToBeProcessed[0] == _SearchTerm: 
+
+            # found the search term
+
+            _PassedList = list() # empty the list and start backtracking
+
+            _PassedList.extend(get_path(_ToBeProcessed))
+
+            return _PassedList
+
+      _PList = bfs(_ToBeProcessed[0], _SearchTerm, _PassedList, _Queue, _Set, _ToBeProcessed[1])
+
+      if _SearchTerm is not None: 
+
+         _PassedList = _PList
+
+         return _PassedList
 
    return _PassedList
 
@@ -108,15 +164,73 @@ def print_bfs(url):
    Print all links reachable from a starting **url** 
    in breadth-first order
    """
+   
+   print "\n" # help make everything more readable
+
    print bfs(url)
     
+   print "\n"
+
+def make_graph(url, _PassedGraph = None, _Queue = None, _Set = None):
+   """
+   Parameters: a url
+   Returns: a graph of all the links using a dictionary to map a url to a list of links
+   """
+
+   if _PassedGraph is None: _PassedGraph = dict()
+   if _Set is None: _Set = set()
+   if _Queue is None: _Queue = deque()
+   
+   _List = getLinks(url) # get all of the links from that url
+   
+   if url not in _Set: 
+
+      ######################################################################
+      # Checking if in set because there may be repeats in the queue
+      ######################################################################
+
+      _PassedGraph[url] = _List
+
+      _Set.add(url)
+
+      _Queue.append(url)
+
+   _Extended = False # boolean to make sure the queue is only added to once
+
+   while _Queue:
+
+      if not _Extended: 
+         
+         _Queue.extend(_List)
+         _Extended = True # recursion will set the value to false on the next call
+
+      _ToBeProcessed = _Queue.popleft() # url to be processed
+
+      if _ToBeProcessed in _Set: continue
+
+      _Set.add(_ToBeProcessed)
+
+      _PassedGraph[_ToBeProcessed] = getLinks(_ToBeProcessed)
+
+      make_graph(_ToBeProcessed, _PassedGraph, _Queue, _Set)
+
+   return _PassedGraph
+
+
 def find_links(url):
    """
    Construct a graph of URLs linked from **url**,
    then use a constant-time operation to return
    all links that url points to.
    """
-   #
+
+   _Graph = make_graph(url)
+
+   print "\n"
+
+   print url + ": " + str(_Graph[url])
+
+   print "\n"
 
 def find_shortest_path(url1,url2):
    """
@@ -124,7 +238,13 @@ def find_shortest_path(url1,url2):
    from **url1** to **url2** if one exists.
    If no such path exists, say so.
    """
-   #
+
+   print "\n"
+
+   print bfs(url1, url2)
+
+   print "\n"
+
 def find_max_depth(url):
    """
    Find and return the "longest shortest path" 
