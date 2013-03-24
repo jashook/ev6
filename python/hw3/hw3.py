59598ee061a6100b4bcce440d836ffd01b99fb70
jashook <jshook@smu.edu>
2013-03-23 22:56:24 -0500

    update

--- python/hw3/hw3.py
+++ python/hw3/hw3.py
@@ -21,57 +21,76 @@ from collections import deque
 ################################################################################
 
 def getLinks(url,baseurl="http://lyle.smu.edu/~tylerm/courses/cse3353/webtraverse/"):
-    """
-    Input: url to visit, Boolean absolute indicates whether URLs should include absolute path (default) or not
-    Output: list of pairs of URLs and associated text
-    """
-    #import the HTML parser package 
-    try:
-        from BeautifulSoup import BeautifulSoup
-    except:
-        print 'You must first install the BeautifulSoup package for this code to work.\n Visit http://lyle.smu.edu/~tylerm/courses/cse3353/pypackages.html for instructions.'
-        raise
-    #fetch the URL and load it into the HTML parser
-    soup = BeautifulSoup(urllib.urlopen(url).read())
-    #pull out the links from the HTML and return
-    return [baseurl+a["href"].encode('ascii','ignore') for a in soup.findAll('a')]
+   """
+   Input: url to visit, Boolean absolute indicates whether URLs should include absolute path (default) or not
+   Output: list of pairs of URLs and associated text
+   """
+   #import the HTML parser package 
+   try:
+      from BeautifulSoup import BeautifulSoup
+   except:
+      print 'You must first install the BeautifulSoup package for this code to work.\n Visit http://lyle.smu.edu/~tylerm/courses/cse3353/pypackages.html for instructions.'
+      raise
+   #fetch the URL and load it into the HTML parser
+   soup = BeautifulSoup(urllib.urlopen(url).read())
+   #pull out the links from the HTML and return
+   return [baseurl+a["href"].encode('ascii','ignore') for a in soup.findAll('a')]
+
+def bfs(url, _Set = None):
+   """
+   Recursive helper function that returns a list of urls
+   in depth-first order
+   """
+   if _Set is None: _Set = set() # create an empty set if null
+
+   _Set.add(url)
+
+   _List = getLinks(url)
+
+   for _Url in _List:
+
+      
+
+   
+
+   
 
 def print_dfs(url):
-    """
-    Print all links reachable from a starting **url** 
-    in depth-first order
-    """
-    #
+   """
+   Print all links reachable from a starting **url** 
+   in depth-first order
+   """
+  
+   print bfs(url)
 
 def print_bfs(url):
-    """
-    Print all links reachable from a starting **url** 
-    in breadth-first order
-    """
-    #
-
+   """
+   Print all links reachable from a starting **url** 
+   in breadth-first order
+   """
+   #
     
 def find_links(url):
-    """
-    Construct a graph of URLs linked from **url**,
-    then use a constant-time operation to return
-    all links that url points to.
-    """
-    #
+   """
+   Construct a graph of URLs linked from **url**,
+   then use a constant-time operation to return
+   all links that url points to.
+   """
+   #
 
 def find_shortest_path(url1,url2):
-    """
-    Find and return the shortest path
-    from **url1** to **url2** if one exists.
-    If no such path exists, say so.
-    """
-    #
+   """
+   Find and return the shortest path
+   from **url1** to **url2** if one exists.
+   If no such path exists, say so.
+   """
+   #
 def find_max_depth(url):
-    """
-    Find and return the "longest shortest path" 
-    from **url** to any other webpage
-    """
-    #
+   """
+   Find and return the "longest shortest path" 
+   from **url** to any other webpage
+   """
+   #
 
 ################################################################################
 # Main
@@ -79,19 +98,18 @@ def find_max_depth(url):
 
 
 if __name__=="__main__":
-    starturl = "http://lyle.smu.edu/~tylerm/courses/cse3353/webtraverse/index.html"
-    print "*********** (a) Depth-first search   **********"
-    print_dfs(starturl)
-    print "*********** (b) Breadth-first search **********"
-    print_bfs(starturl)
-    print "*********** (c) Constant-time lookup of links for given URL once graph is made *****"
-    find_links("http://lyle.smu.edu/~tylerm/courses/cse3353/webtraverse/blink.html
-")
-    print "*********** (d) Find shortest path between two URLs ********"
-    find_shortest_path("http://lyle.smu.edu/~tylerm/courses/cse3353/webtraverse/index.html","http://lyle.smu.edu/~tylerm/courses/cse3353/webtraverse/matula.html")
-    find_shortest_path("http://lyle.smu.edu/~tylerm/courses/cse3353/webtraverse/turing.html","http://lyle.smu.edu/~tylerm/courses/cse3353/webtraverse/dijkstra.html")
-    print "*********** (e) Find the longest shortest path from a starting URL *****"
-    find_max_depth(starturl)
+   starturl = "http://lyle.smu.edu/~tylerm/courses/cse3353/webtraverse/index.html"
+   print "*********** (a) Depth-first search   **********"
+   print_dfs(starturl)
+   print "*********** (b) Breadth-first search **********"
+   print_bfs(starturl)
+   print "*********** (c) Constant-time lookup of links for given URL once graph is made *****"
+   find_links("http://lyle.smu.edu/~tylerm/courses/cse3353/webtraverse/blink.html")
+   print "*********** (d) Find shortest path between two URLs ********"
+   find_shortest_path("http://lyle.smu.edu/~tylerm/courses/cse3353/webtraverse/index.html","http://lyle.smu.edu/~tylerm/courses/cse3353/webtraverse/matula.html")
+   find_shortest_path("http://lyle.smu.edu/~tylerm/courses/cse3353/webtraverse/turing.html","http://lyle.smu.edu/~tylerm/courses/cse3353/webtraverse/dijkstra.html")
+   print "*********** (e) Find the longest shortest path from a starting URL *****"
+   find_max_depth(starturl)
 
 
 ################################################################################
