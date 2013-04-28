0f501fb6affbaee78b92150df7e2cd7ce0cc36eb
jashook <jshook@smu.edu>
2013-04-27 21:19:59 -0500

    finished insert

--- assembly/search_engine/include/bn_tree.asm
+++ assembly/search_engine/include/bn_tree.asm
@@ -18,39 +18,49 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
+%include "alloc.asm"
+%include "io.asm"
+%include "string.asm"
+%include "util.asm"
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 section .data
 
-length         equ   81
+; nothing to define
 
-message        db    "Hello", 0
-message2       db    " World", 10, 0
-
-_Head          dd    0h, 0h, 0h
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 section .bss
 
+; nothing to reserve
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 section .text
 
-global _start                                      ; declared for linker
-
-extern alloc
-extern print_str
-extern _system_exit_
-extern str_len
-
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+global _start                    ; declared for linker
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;
-;  Procedure: create_node
+; Procedure: create_node
 ;
-;  Arguements:
+; Arguements:
 ;
 ;     VOID
+;
+; Uses:
+;
+;     EAX
 ;  
-;  Returns:
+; Returns:
 ;
 ;     EAX: pointer to the node
 ;
@@ -59,11 +69,259 @@ extern str_len
 
 create_node:
 
-         mov   eax, 32 * 3          ; size of a node
+         mov   eax, 32 * 3       ; size of a node
          
-         call  alloc                ; call allocate
+         call  alloc             ; call allocate
 
          ret
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;
+; Procedure: create_tree
+;
+; Arguements:
+;
+;     VOID
+;
+; Uses:
+;
+;     EAX
+;
+; Returns:
+;
+;     EAX: pointer to a tree structure
+;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+create_tree:
+
+         mov   eax, 32 * 2       ; struct to hold a head pointer and the size of the tree
+
+         mov   ebx, eax
+
+         call  create_node       ; create the head
+
+         mov   [eax], dword 0    ; zero out the data
+
+         mov   [eax + 4], dword 0   ; zero out the right pointer
+
+         mov   [eax + 8], dword 0   ; zero out the right pointer
+
+         mov   [ebx], dword eax  ; head at the first index
+
+         mov   [ebx + 4], dword 0   ; zero out the size
+
+         mov   eax, ebx
+
+         ret
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;
+; Procedure: descend_tree
+;
+; Arguements:
+;
+;     EBX: pointer to the head node
+;     ECX: string to be inserted
+;
+; Uses:
+;
+;     EAX
+;     EBX
+;     ECX
+;     EDX
+;
+; Returns:
+;
+;     EBX: pointer to node to be inserted at
+;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+descend_tree:
+
+         mov   eax, ebx          ; descending pointer = eax = head
+
+         mov   edx, eax          ; descending parent pointer = descending pointer
+
+descend_loop:
+
+         cmp   eax, dword 0      ; is the descending pointer null?
+
+         je    exit_descend_loop ; then exit the loop
+
+         push  eax               ; save the pointer
+
+         mov   ebx, ecx          ; first string
+
+         mov   ecx, [eax]        ; second string
+
+         call  str_cmp
+
+         cmp   eax, 0            ; are the strings equal?
+
+         pop   eax               ; get back the descending pointer
+
+         jb    less_than_desc 
+
+         ja    greater_than_desc
+
+         je    equal_to_desc
+
+less_than_desc:
+
+         mov   edx, eax          ; descending parent pointer = descending pointer
+
+         mov   eax, [eax + 4]    ; descending pointer = the left child
+
+         jmp   descend_loop      ; continue descending
+
+greater_than_desc:
+
+         mov   edx, eax          ; descending parent pointer = descending pointer
+
+         mov   eax, [eax + 8]    ; descending pointer = the right child
+
+         jmp   descend_loop      ; continue descending
+
+equal_to_desc:
+
+         mov   edx, eax          ; set the parent pointer to the descending pointer
+
+                                 ; fall through
+
+exit_descend_loop:
+
+         mov   ebx, edx          ; set up to return the parent pointer
+
+         ret
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;
+; Procedure: insert_tree
+;
+; Arguements:
+;
+;     EAX: pointer to a tree structure
+;     ECX: string to be inserted
+;
+; Uses:
+;
+;     EAX
+;     EBX
+;     ECX
+;
+; Returns:
+;
+;     VOID
+;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+insert_tree:
+
+         mov   ebx, dword [eax]  ; move into ebx the pointer to the head node
+
+         cmp   [ebx], dword 0    ; is the data for the head null?
+
+         je    insert_at_head    ; then insert at the head
+
+         push  eax               ; save eax
+
+         push  ecx               ; save ecx
+
+         call  descend_tree      ; descend through the tree if the head is already set
+
+         pop   ecx               ; restore ecx
+
+         pop   eax               ; restore eax
+
+         mov   edx, ebx
+
+         mov   ebx, ecx          ; first string
+
+         mov   ecx, edx          ; second string
+ 
+         push  eax
+
+         call  str_cmp
+
+         cmp   eax, 0            ; are they equal?
+
+         pop eax
+
+         mov   ecx, ebx
+
+         mov   ebx, edx
+   
+         jb    insert_left       ; if less than insert at the left
+
+         ja    insert_right      ; else insert at the right
+
+return_insert_tree:
+
+         ret
+
+insert_at_head:
+
+         mov   [ebx], dword ecx  ; insert the string
+
+         inc   [eax + 4]         ; increment the size
+
+         ret
+
+insert_left:
+
+         push  eax               ; save eax
+
+         pushad                  ; save the registers
+
+         call create_node
+
+         popad                   ; restore the registers
+
+         mov   [eax], ecx        ; insert the string
+
+         mov   [eax + 4], dword 0   ; zero out the left pointer
+
+         mov   [eax + 8], dword 0   ; zero out the right pointer
+
+         mov   [ebx + 4], dword eax ; set the new node to be the left pointer
+   
+         pop   eax               ; restor eax
+
+         inc   [eax + 4]         ; increment the size of the tree
+
+         jmp   return_insert_tree
+
+insert_right:
+
+         push  eax               ; save eax
+
+         pushad                  ; save the registers
+
+         call create_node
+
+         popad                   ; restore the registers
+
+         mov   [eax], ecx        ; insert the string
+
+         mov   [eax + 4], dword 0   ; zero out the left pointer
+
+         mov   [eax + 8], dword 0   ; zero out the right pointer
+
+         mov   [ebx + 8], dword eax ; set the new node to be the right pointer
+   
+         pop   eax               ; restor eax
+
+         inc   [eax + 4]         ; increment the size of the tree
+      
+         jmp   return_insert_tree
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -72,7 +330,7 @@ create_node:
 ;
 ;  Arguements:
 ;
-;     EAX: tree root
+;     EAX: pointer to the root of a tree
 ;
 ;  Returns:
 ;
@@ -119,36 +377,3 @@ _return_:
 
          ret
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-; 
-; Procedure: _run
-;
-; Arguements:
-;
-;     VOID
-;
-;  Returns:
-;
-;     VOID
-;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-_run:                                        ; entry point
-
-         nop
-
-         mov   [_Head], dword message        ; store the address of message, in the "node"
-         
-         call create_node                    ; allocate space for a new node
-
-         mov   [eax], dword message2         ; eax stores the address of the node
-
-         mov   [_Head + 4], dword eax        ; 4 bytes (offset for a dword)
-
-         mov   eax, _Head
-
-         call print_tree;
-
-         call _system_exit_
